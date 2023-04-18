59b23a5a1ef0894f5cea1a727c34213981fed9e7
MarmoW <115847012+MarmoW@users.noreply.github.com>
2023-04-18 06:56:08 -0300

    payments

--- src/controllers/tickets-controller.ts
+++ src/controllers/tickets-controller.ts
@@ -8,7 +8,7 @@ export async function getTicketType(_req: Request, res: Response) {
     const type = await ticketsService.getTicketTypes();
     return res.status(httpStatus.OK).send(type);
   } catch (err) {
-    return res.status(httpStatus.NOT_FOUND).send({});
+    return res.status(httpStatus.UNAUTHORIZED).send([]);
   }
 }
 
@@ -19,7 +19,7 @@ export async function getTicket(req: AuthenticatedRequest, res:Response){
         const tickets = await ticketsService.getTickets(req.userId);
         res.status(httpStatus.OK).send(tickets);
     }catch(err){
-      return res.status(httpStatus.NOT_FOUND).send({});
+      return res.status(httpStatus.UNAUTHORIZED).send([]);
     }
 }
 
@@ -29,6 +29,6 @@ export async function postTicket(req: AuthenticatedRequest, res: Response){
         const tickets = await ticketsService.postTickets(req.userId, req.body.ticketTypeId);
         res.status(httpStatus.OK).send(tickets);
     }catch(err){
-      return res.status(httpStatus.NOT_FOUND).send({});
+      return res.status(httpStatus.UNAUTHORIZED).send([]);
     }
 }
